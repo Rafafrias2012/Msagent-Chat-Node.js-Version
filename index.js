1bf010f6b7db2e32b58e875cecd5f43f14c3296f
ItzRafaelScout <155685529+Rafafrias2012@users.noreply.github.com>
2024-12-21 20:43:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const Fastify = require('fastify');
-const FastifyWS = require('@fastify/websocket');
-const FastifyStatic = require('@fastify/static');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
 const Client = require('./client');
 
-const app = Fastify({ logger: true, });
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.register(FastifyWS);
-app.register(FastifyStatic, {
-  root: './frontend', // serve files from the frontend folder
-  prefix: '/', // serve files at the root URL
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+wss.on('connection', (ws, req) => {
+    let client = new Client(ws);
 });
 
-app.get("/socket", {websocket: true}, (socket, req) => {
-  let client = new Client(socket);
-});
-
-let port;
-if (process.argv.length < 3 || isNaN(port = parseInt(process.argv[2]))) {
-  console.error("Usage: index.js [port]");
-  process.exit(1);
+let port = process.argv[2];
+if (!port || isNaN(port = parseInt(port))) {
+    console.error("Usage: node index.js [port]");
+    process.exit(1);
 }
-app.listen({host: "127.0.0.1", port});
+
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Server is listening on port ${port}`);
+});

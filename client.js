595f2ed2c973b9c80c55bb2d38fd4c6fb5092e4b
ItzRafaelScout <155685529+Rafafrias2012@users.noreply.github.com>
2024-12-21 20:52:00 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,27 +1,56 @@
 const EventEmitter = require('events');
 const WebSocket = require('ws');
+const { MSAgentProtocolMessageType } = require('./protocol.js'); // Adjust the path as necessary
+const { MSAgentChatRoom } = require('./room.js');
 
 class Client extends EventEmitter {
-  constructor(socket) {
-    super();
-    this.username = null;
-    this.socket = socket;
-    this.socket.on('message', (msg, isBinary) => {
-      if (isBinary) {
-        this.socket.close();
-        return;
-      }
-      this.parseMessage(msg.toString('utf-8'));
-    });
-    this.socket.on('error', () => {});
-    this.socket.on('close', () => {
-      this.emit('close');
-    });
-  }
+    constructor(socket, room) {
+        super();
+        this.socket = socket;
+        this.room = room;
+        this.username = null;
+        this.socket.on('message', (msg, isBinary) => {
+            if (isBinary) {
+                this.socket.close();
+                return;
+            }
+            this.parseMessage(msg.toString('utf-8'));
+        });
+        this.socket.on('error', () => {});
+        this.socket.on('close', () => {
+            this.emit('close');
+        });
+    }
 
-  parseMessage(msg) {
+    send(msg) {
+        return new Promise((res, rej) => {
+            this.socket.send(JSON.stringify(msg), err => {
+                if (err) {
+                    rej(err);
+                    return;
+                }
+                res();
+            });
+        });
+    }
 
-  }
+    parseMessage(data) {
+        let msg;
+        try {
+            msg = JSON.parse(data);
+        } catch {
+            this.socket.close();
+            return;
+        }
+        switch (msg.op) {
+            case MSAgentProtocolMessageType.Join: {
+                break;
+            }
+            case MSAgentProtocolMessageType.Talk: {
+                break;
+            }
+        }
+    }
 }
 
 module.exports = Client;
